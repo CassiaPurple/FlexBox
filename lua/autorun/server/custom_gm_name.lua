0bcecbe7b68c91daeb33d9a54f450b774d62d084
Flex <LUModder@gmail.com>
2015-04-20 15:38:44 -0600

    Changed to Sandbox: QBox to be along with Meta in the serverlist :B1:

--- lua/autorun/server/custom_gm_name.lua
+++ lua/autorun/server/custom_gm_name.lua
@@ -1,5 +1,5 @@
 local sv_gamename = CreateConVar ("sv_gamename", "", {FCVAR_NOTIFY, FCVAR_ARCHIVE, FCVAR_GAMEDLL})
 
 hook.Add ('GetGameDescription', "custdesc", function ()
-	return "FlexBox (QBox)"
+	return "Sandbox: QBox"
 end)
